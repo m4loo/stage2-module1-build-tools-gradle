0d66243ccf4ff39d742cd162defb738945aa70d8
m4loo <128167781+m4loo@users.noreply.github.com>
2023-08-17 19:21:28 +0600

    Revert "done?"
    This reverts commit 5c91e87523e2fa4a1047b487dd436a3fd3302a06.

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -4,6 +4,6 @@ import java.util.List;
 
 public class Utils {
     public static boolean isAllPositiveNumbers(List<String> args) {
-        return args.stream().allMatch(StringUtils::isPositiveNumber);
+        //magic happens here
     }
 }
