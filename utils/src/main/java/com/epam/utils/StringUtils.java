7fc8beb0230b6bf21ab94933f013efd1f10f0e5b
m4loo <128167781+m4loo@users.noreply.github.com>
2023-08-17 19:21:41 +0600

    Revert "done?"
    This reverts commit 75e686ab496c5639d0377fedf1dc4bb6a5b731f0.

--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -2,9 +2,6 @@ package com.epam.utils;
 
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
-        if (org.apache.commons.lang3.StringUtils.isNumeric(str)){
-            int num = Integer.parseInt(str);return num > 0;
-        } else {
-            return false;
+         //here magic will happen
     }
 }
