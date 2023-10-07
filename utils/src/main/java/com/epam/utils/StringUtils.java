865cba4a4dd5562e635834bdfebb031e34d38f28
m4loo <128167781+m4loo@users.noreply.github.com>
2023-10-07 12:27:20 +0600

    done??

--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -2,8 +2,23 @@ package com.epam.utils;
 
 import org.apache.commons.lang3.StringUtils;
 
+//public class StringUtils {
+//    public static boolean isPositiveNumber(String str) {
+//        return StringUtils.isNumeric(str) && Integer.parseInt(str) > 0;
+//    }
+//}
+
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
-        return StringUtils.isNumeric(str) && Integer.parseInt(str) > 0;
+        if (str == null || str.isEmpty()) {
+            return false;
+        }
+
+        try {
+            int num = Integer.parseInt(str);
+            return num > 0;
+        } catch (NumberFormatException e) {
+            return false;
+        }
     }
 }
